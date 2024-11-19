43b5d042d11c4ed8df0840e9712100f200cce09c
Johanna-15 <johannamariesaldua10@gmail.com>
2024-11-19 15:39:15 +0800

    delete_task.php

--- delete_task.php
+++ delete_task.php
@@ -1,8 +1,8 @@
 <?php
-include 'indexJ.php';
+include 'index.php';
 
 $id = $_GET['id'];
 $sql = "DELETE FROM tasks WHERE id=$id";
 $conn->query($sql);
-header("Location: indexJ.php");
+header("Location: index.php");
 ?>
